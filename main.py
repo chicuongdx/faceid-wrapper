530aadda3c187be738f0e5b95320f1eaebd264e8
chicuongdx <chicuongdx2001@gmail.com>
2022-11-24 19:07:39 +0700

    main

--- main.py
+++ main.py
@@ -136,5 +136,5 @@ async def face_indentity(img: UploadFile = File(...)):
     except Exception as e:
         return {"msg": e, "status": "fail"}
 
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
